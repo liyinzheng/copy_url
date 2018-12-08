70d3aff1be8ac26c7132e7b5ab04a188af36f836
lyz <446784359@qq.com>
2018-12-08 14:42:10 +0800

    代码结构优化与项目说明更新

--- js/content-script.js
+++ js/content-script.js
@@ -7,67 +7,131 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 function getLANIP(callback) {
-    var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
-    if (RTCPeerConnection)(function () {
-        var rtc = new RTCPeerConnection({
-            iceServers: []
+    let RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+    if (RTCPeerConnection) {
+        let rtc = new RTCPeerConnection({
+            iceServers: [],
         });
-        if (1 || window.mozRTCPeerConnection) {
-            rtc.createDataChannel('', {
-                reliable: false
-            });
-        };
+
+        rtc.createDataChannel('', {
+            reliable: false
+        });
 
         rtc.onicecandidate = function (evt) {
-            if (evt.candidate) grepSDP("a=" + evt.candidate.candidate);
+            if (evt.candidate) {
+                grepSDP("a=" + evt.candidate.candidate);
+            }
         };
-        rtc.createOffer(function (offerDesc) {
+
+        rtc.createOffer(offerDesc => {
             grepSDP(offerDesc.sdp);
             rtc.setLocalDescription(offerDesc);
-        }, function (e) {
+        }, e => {
             console.warn("offer failed", e);
         });
 
-
-        var addrs = Object.create(null);
+        let addrs = Object.create(null);
         addrs["0.0.0.0"] = false;
 
         function updateDisplay(newAddr) {
-            if (newAddr in addrs) return;
-            else addrs[newAddr] = true;
-            var displayAddrs = Object.keys(addrs).filter(function (k) {
-                return addrs[k];
-            });
-            for (var i = 0; i < displayAddrs.length; i++) {
+            if (newAddr in addrs) {
+                return;
+            } else {
+                addrs[newAddr] = true;
+            }
+            let displayAddrs = Object.keys(addrs).filter(k => addrs[k]);
+            for (let i = 0; i < displayAddrs.length; i++) {
                 if (displayAddrs[i].length > 16) {
                     displayAddrs.splice(i, 1);
                     i--;
                 }
             }
-            // console.log(displayAddrs[0]); //打印出内网ip
             if (callback) {
                 callback(displayAddrs[0]);
             }
         }
 
         function grepSDP(sdp) {
-            var hosts = [];
-            sdp.split('\r\n').forEach(function (line, index, arr) {
+            sdp.split('\r\n').forEach(line => {
                 if (~line.indexOf("a=candidate")) {
-                    var parts = line.split(' '),
+                    let parts = line.split(' '),
                         addr = parts[4],
                         type = parts[7];
-                    if (type === 'host') updateDisplay(addr);
+                    if (type === 'host') {
+                        updateDisplay(addr);
+                    }
                 } else if (~line.indexOf("c=")) {
-                    var parts = line.split(' '),
+                    let parts = line.split(' '),
                         addr = parts[2];
                     updateDisplay(addr);
                 }
             });
         }
-    })();
-
+    }
 }
+
+// function getLANIP(callback) {
+//     var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+//     if (RTCPeerConnection)(function () {
+//         var rtc = new RTCPeerConnection({
+//             iceServers: []
+//         });
+//         if (1 || window.mozRTCPeerConnection) {
+//             rtc.createDataChannel('', {
+//                 reliable: false
+//             });
+//         };
+
+//         rtc.onicecandidate = function (evt) {
+//             if (evt.candidate) grepSDP("a=" + evt.candidate.candidate);
+//         };
+//         rtc.createOffer(function (offerDesc) {
+//             grepSDP(offerDesc.sdp);
+//             rtc.setLocalDescription(offerDesc);
+//         }, function (e) {
+//             console.warn("offer failed", e);
+//         });
+
+
+//         var addrs = Object.create(null);
+//         addrs["0.0.0.0"] = false;
+
+//         function updateDisplay(newAddr) {
+//             if (newAddr in addrs) return;
+//             else addrs[newAddr] = true;
+//             var displayAddrs = Object.keys(addrs).filter(function (k) {
+//                 return addrs[k];
+//             });
+//             for (var i = 0; i < displayAddrs.length; i++) {
+//                 if (displayAddrs[i].length > 16) {
+//                     displayAddrs.splice(i, 1);
+//                     i--;
+//                 }
+//             }
+//             // console.log(displayAddrs[0]); //打印出内网ip
+//             if (callback) {
+//                 callback(displayAddrs[0]);
+//             }
+//         }
+
+//         function grepSDP(sdp) {
+//             var hosts = [];
+//             sdp.split('\r\n').forEach(function (line, index, arr) {
+//                 if (~line.indexOf("a=candidate")) {
+//                     var parts = line.split(' '),
+//                         addr = parts[4],
+//                         type = parts[7];
+//                     if (type === 'host') updateDisplay(addr);
+//                 } else if (~line.indexOf("c=")) {
+//                     var parts = line.split(' '),
+//                         addr = parts[2];
+//                     updateDisplay(addr);
+//                 }
+//             });
+//         }
+//     })();
+
+// }
 
 function getMyUrl() {
     let i, targetIp,
